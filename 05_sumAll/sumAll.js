18ea99d569d091bd20f092c6d9269ec67cc87362
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-09 23:27:06 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!(Number.isInteger(x) && Number.isInteger(y)) ||
+        x < 0 || y < 0) {
+        return 'ERROR';
+    }
+    let start = Math.min(x, y);
+    let end = Math.max(x, y);
+    let result = 0;
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
