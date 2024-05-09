5d020ef542a2516f2217a596c411cc8388d49921
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-09 23:15:03 +0200

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    array = array.filter((number) => !(args.includes(number)));
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
