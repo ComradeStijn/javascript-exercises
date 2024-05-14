8b63255daafca67548baa50feb94665871e91664
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-15 00:54:23 +0200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
 
+
+
+const palindromes = function (string) {
+    const delimiters = /[!?;.,|\s]/
+    const lowerString = string
+                        .split(delimiters)
+                        .join("")
+                        .toLowerCase();
+    const reversedString = lowerString.split("").reverse().join("");
+
+    return (reversedString === lowerString);
 };
 
 // Do not edit below this line
