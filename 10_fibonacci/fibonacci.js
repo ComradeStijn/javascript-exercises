c6be67c2dbc19a7d2c30d39b33238cf4dd5c27f7
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-15 01:07:37 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
 
+
+
+const fibonacci = function(index) {
+    if (typeof index === 'string') {
+        index = parseInt(index);
+    }
+
+    if (index < 0) return "OOPS";
+    if (index <= 1) return index;
+    return fibonacci(index - 1) + fibonacci(index - 2);
 };
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
