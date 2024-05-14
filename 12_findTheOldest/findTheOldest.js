2affee004ea3f903e00c5504ee99ae030c86fdc2
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-15 01:19:29 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.sort((a, b) => {
+        if (!a.yearOfDeath) a.yearOfDeath = new Date().getFullYear();
+        if (!b.yearOfDeath) b.yearOfDeath = new Date().getFullYear();
+        return (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1);
+    })[0];
 };
 
+const people = [
+    {
+        name: "st",
+        yearOfBirth: 1997,
+    },
+    {
+        name: 'jl',
+        yearOfBirth: 1996,
+    },
+]
+
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
